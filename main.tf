eb3d006be5490b7a1fe5b6709373e01704d9dc9d
kuzminoo84 <kuzminovalesia@gmail.com>
2025-08-29 15:07:50 -0400

    expand

--- main.tf
+++ main.tf
@@ -18,19 +18,49 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "4.13.0"
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+
+   
+  subnet_id = module.blog_vpc.public_subnets[0]
   tags = {
     Name = "Learning Terraform"
   }
 }
 
+<<<<<<< HEAD
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  tags = {
+    Terraform = "true"
+  }
+  vpc_id = data.aws_vpc.default.id
+}
+=======
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
+>>>>>>> e04e137778dcb5a3d2f385d7f524ea147eb119cf
 
   vpc_id  = data.aws_vpc.default.id
   name    = "blog"
